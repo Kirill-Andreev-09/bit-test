c1be7516cca3b16baf181d293295979e8e75b9a5
yoyohoho84 <kirill_1131@mail.ru>
2024-01-09 20:35:54 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    define: {
+      "process.env.CONTACT_PHONE": JSON.stringify(env.CONTACT_PHONE),
+    },
+    server: {
+      port: 8080,
+      open: true,
+      host: "0.0.0.0",
+    },
+    preview: {
+      port: 8080,
+      open: false,
+    },
+  };
+});
